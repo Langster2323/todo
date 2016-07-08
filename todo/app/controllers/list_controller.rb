a4cbc735abcb4b29f5845dde2f059dac6c0ac004
Ahkeem Lang <langahkeem@gmail.com>
2016-07-08 14:30:58 -0500

    Working on my router

--- todo/app/controllers/list_controller.rb
+++ todo/app/controllers/list_controller.rb
@@ -4,11 +4,11 @@ class ListController < ApplicationController
   end
 
   def show
-    if user_exist?
-      render User.all[user_id].to_json
-    else
-      render_not_found
-    end
+    # if user_exist?
+    #   render User.all[user_id].to_json
+    # else
+    #   render_not_found
+    # end
   end
 
   def create
